96495dbebe333b4688d360730113cc9dbfea240e
Ryan Williams <ryanwilliams@Ryans-MacBook-Pro.local>
2022-10-01 12:23:12 -0400

    Updates

--- web/cypress.config.js
+++ web/cypress.config.js
@@ -1,8 +1,20 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  retries:{ "runMode": 2, "openMode": 1},
+  videoUploadOnPasses:true,
+  video:true,
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: 'Drata Web Test',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: true,
+  },
   e2e: {
     setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
       // implement node event listeners here
     },
   },
